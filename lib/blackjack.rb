9f9e8e8541dd897ecf89aaaa7f707bcc82d5268b
jaspercurry <jaspercurry@gmail.com>
2016-02-26 17:39:31 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,57 @@
 def welcome
+  puts "Welcome to the Blackjack Table"
+
   # code #welcome here
 end
 
 def deal_card
+  rand(1..11)
+
   # code #deal_card here
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+  x = deal_card + deal_card
+  display_card_total(x)
+  return x
 end
 
-def hit?
+def hit?(x)
+  prompt_user
+  y = get_user_input
+  if y == "h"
+      return deal_card + x
+  elsif y == "s"
+    return x
+  else
+  invalid_command
+
+  end
   # code hit? here
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+
+  #puts
 end
 
 #####################################################
@@ -39,6 +59,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  first_total = initial_round
+  second_total = hit?(first_total)
+  if second_total > 21
+    end_game(second_total)
+  else
+    hit(second_total)
+  end
+   display_card_total(second_total)
 end
     
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,11 @@
 require_relative "blackjack.rb"
 
+def runner
+  welcome
+  initial_round
+
+  prompt_user
+  hit?
+end
+
 runner
